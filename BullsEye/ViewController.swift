dcb0b2de2b5705e4c811355ca9ab1edd312f7d5e
William Bard <wdbard@gmail.com>
2022-01-17 12:01:22 -0500

    Fixed About View Controller (added constraints, changed Aspect Fit to Aspect Fill)

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -86,6 +86,13 @@ class ViewController: UIViewController {
         score = 0
         round = 0
         startNewRound()
+        
+        let transition = CATransition()
+        transition.type = CATransitionType.fade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(
+            name: CAMediaTimingFunctionName.easeOut)
+        view.layer.add(transition, forKey: nil)
     }
     
     @IBOutlet var targetLabel: UILabel!
